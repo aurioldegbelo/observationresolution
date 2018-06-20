a8a8d685ab2332c449fae5d5c8a03e7dd1440f57
aurioldegbelo <degbelo@uni-muenster.de>
2018-06-20 07:34:57 +0200

    Update ObservationCollectionResolution.hs

--- ObservationCollectionResolution.hs
+++ ObservationCollectionResolution.hs
@@ -1,35 +1,55 @@
 {-# LANGUAGE MultiParamTypeClasses #-} 
 
+{-  
+	 Haskell specification presented in the fifth chapter of 'Spatial and Temporal Resolution of Sensor Observations'
+	 The scope of the specification is limited to the resolution of an observation collection
+     +++ Main modelling choices +++ 
+   - observation collections as lists
+   - spatial resolution as the observed study area (i.e. portion of the study area that has been observed)
+   - the observed study area is the sum of the distinct observed areas of the observations which are members of the collection
+   - the observed area of an observation is the spatial region of the phenomenon of interest that has been observed. It can be estimated using the 
+     spatial receptive field or the spatial location of the observer. The examples presented in this specification take 'observed area' of an 
+	 observation as the spatial location of the observer
+   - temporal resolution as the observed study period (i.e. portion of the study period that has been observed)
+   - the observed study period is the sum of the distinct observed periods of the observations which are members of the collection
+   - the observed period of an observation is the temporal region of the phenomenon of interest that has been observed. It can be estimated using the 
+     temporal receptive window or the temporal location of the observer. The examples presented in this specification take 'observed period' of an 
+	 observation as the temporal location of the observer
+	 Last modified: August 16, 2013
+-}
+
 module ObservationCollectionResolution where
 
 import Data.List
 type Id = Int -- an identifier
 
+type Area = Int -- spatial area as Int
+type Duration = Int -- temporal duration as Int
+type Value = Double -- value as Double
+
+type ObserverName = String -- observer name as String
+
 -- introduction of DOLCE's classes space_region and temporal_region
-class ABSTRACT abstract
-class ABSTRACT region => REGION region
-class REGION temporalRegion => TEMPORAL_REGION temporalRegion -- a temporal region is a region
-class REGION physicalRegion => PHYSICAL_REGION physicalRegion
-class PHYSICAL_REGION spaceRegion => SPACE_REGION spaceRegion -- a space region is a region
+class ABSTRACTS abstract
+class ABSTRACTS region => REGIONS region
+class REGIONS temporalRegion => TEMPORAL_REGIONS temporalRegion -- a DOLCE temporal region (temporal region is a region)
+class REGIONS physicalRegion => PHYSICAL_REGIONS physicalRegion
+class PHYSICAL_REGIONS spaceRegion => SPACE_REGIONS spaceRegion -- a DOLCE space region (space region is a region)
 
--- qualities
-class QUALITY observationcollection
-class QUALITY observationcollection => ABSTRACT_QUALITY observationcollection -- abstract qualities
-
--- non-physical endurants
+-- non-physical endurants and social objects
 class NON_PHYSICAL_ENDURANTS nonPhysicalEndurant
 class NON_PHYSICAL_ENDURANTS nonPhysicalObject => NON_PHYSICAL_OBJECTS nonPhysicalObject
 class NON_PHYSICAL_OBJECTS socialObject => SOCIAL_OBJECTS socialObject -- social object
 
 -- a spatial region has an id and a size 
-data SpatialRegion = SpatialRegion {srId:: Id, srSize :: Double} deriving Show
+data SpatialRegion = SpatialRegion {srId:: Id, srSize :: Area} deriving Show
 spatialregion1 = SpatialRegion {srId=1, srSize= 100} 
 spatialregion2 = SpatialRegion {srId=2, srSize= 100}
 spatialregion3 = SpatialRegion {srId=3, srSize= 100}
 unknownSpatialRegion = Nothing
 
 -- a temporal region has an id and a size 
-data TemporalRegion =  TemporalRegion {trId:: Id, trSize :: Double} deriving Show
+data TemporalRegion =  TemporalRegion {trId:: Id, trSize :: Duration} deriving Show
 temporalregion1 = TemporalRegion {trId=1, trSize= 60}
 temporalregion2 = TemporalRegion {trId=2, trSize= 60}
 temporalregion3 = TemporalRegion {trId=3, trSize= 60}
@@ -40,47 +60,49 @@ temporalregion7 = TemporalRegion {trId=7, trSize= 60}
 unknownTemporalRegion = Nothing
 
 -- a spatial region is a (DOLCE) space region  
-instance ABSTRACT SpatialRegion
-instance REGION SpatialRegion
-instance PHYSICAL_REGION SpatialRegion
-instance SPACE_REGION SpatialRegion
+instance ABSTRACTS SpatialRegion
+instance REGIONS SpatialRegion
+instance PHYSICAL_REGIONS SpatialRegion
+instance SPACE_REGIONS SpatialRegion
 
 -- a temporal region is a (DOLCE) temporal region
-instance ABSTRACT TemporalRegion
-instance REGION TemporalRegion
-instance TEMPORAL_REGION TemporalRegion
+instance ABSTRACTS TemporalRegion
+instance REGIONS TemporalRegion
+instance TEMPORAL_REGIONS TemporalRegion
 
--- an observation has an id, and a value; it was produced by a sensor
-data Observation =  Observation {obsId:: Id, sensor:: String, obsValue:: String} deriving Show
+-- an observation has an id, and a value; it was produced by an observer (the observer is identified here by its name)
+data Observation =  Observation {obsId:: Id, observer:: ObserverName, obsValue:: Value} deriving Show
 
 -- an observation is a social object
 instance NON_PHYSICAL_ENDURANTS Observation
 instance NON_PHYSICAL_OBJECTS Observation
 instance SOCIAL_OBJECTS Observation
 
--- b1 is produced by the sensor B and has the value of 2.8 ppm (parts per million) 
-b1= Observation {obsId =1, sensor = "B", obsValue = "2.8"}
-b2= Observation {obsId =2, sensor = "B", obsValue = "2.9"}
-b3= Observation {obsId =3, sensor = "B", obsValue = "2.5"}
-b4= Observation {obsId =4, sensor = "B", obsValue = "2.6"}
-b5= Observation {obsId =5, sensor = "B", obsValue = "2.3"}
+-- b1 is produced by the observer B and has the value of 2.8 ppm (parts per million) 
+b1= Observation {obsId =1, observer = "B", obsValue = 2.8}
+b2= Observation {obsId =2, observer = "B", obsValue = 2.9}
+b3= Observation {obsId =3, observer = "B", obsValue = 2.5}
+b4= Observation {obsId =4, observer = "B", obsValue = 2.6}
+b5= Observation {obsId =5, observer = "B", obsValue = 2.3}
 
-c1= Observation {obsId =6, sensor = "C", obsValue = "4"}
+c1= Observation {obsId =6, observer = "C", obsValue = 4}
 
-e1= Observation {obsId =7, sensor = "E", obsValue = "3.5"}
-e2= Observation {obsId =8, sensor = "E", obsValue = "3.3"}
+e1= Observation {obsId =7, observer = "E", obsValue = 3.5}
+e2= Observation {obsId =8, observer = "E", obsValue = 3.3}
 
--- location is an abstract quality
-class ABSTRACT_QUALITY observation => LOCATED observation where
+-- definition of location  
+class LOCATED observation where
 	-- spatial location as a relation between an observation and a spatial region
 	spatialLocation :: observation -> SpatialRegion 
 	-- temporal location as a relation between an observation and a temporal region
 	temporalLocation :: observation -> TemporalRegion
 
--- spatial and temporal locations are arbitrarily assigned to the different observations	
+-- spatial and temporal locations are assigned to the different observations (spatial/temporal location of an observation is equal to the spatial/temporal location
+-- of the observer. For simplicity, the locations of the observers are not represented in this example. The functions below provide only a mechanism to 
+-- ascribe a spatial/temporal region to the observation)	
 instance LOCATED Observation where
-	spatialLocation Observation{obsId=oId, sensor=obsSensor, obsValue = oValue} =  SpatialRegion{srId = if(obsSensor == "B") then 1 else (if obsSensor == "C" then 2 else 3), srSize=100}
-	temporalLocation Observation{obsId=oId, sensor=obsSensor, obsValue = oValue} = TemporalRegion{trId = if (obsSensor == "B" && oId == 1) then 1 else (if (obsSensor == "B" && oId == 2) then 2 else if (obsSensor == "B" && oId == 3) then 3 else if (obsSensor == "B" && oId == 4) then 4 else if (obsSensor == "B" && oId == 5) then 5 else if (obsSensor == "C") then 3 else if (obsSensor == "E" && oId == 7) then 6 else 7), trSize = 60}
+	spatialLocation Observation{obsId=oId, observer=obsSensor, obsValue = oValue} =  SpatialRegion{srId = if(obsSensor == "B") then 1 else (if obsSensor == "C" then 2 else 3), srSize=100}
+	temporalLocation Observation{obsId=oId, observer=obsSensor, obsValue = oValue} = TemporalRegion{trId = if (obsSensor == "B" && oId == 1) then 1 else (if (obsSensor == "B" && oId == 2) then 2 else if (obsSensor == "B" && oId == 3) then 3 else if (obsSensor == "B" && oId == 4) then 4 else if (obsSensor == "B" && oId == 5) then 5 else if (obsSensor == "C") then 3 else if (obsSensor == "E" && oId == 7) then 6 else 7), trSize = 60}
 -- some examples of spatial location 
 slB1 = spatialLocation b1
 slB2 = spatialLocation b2
@@ -100,20 +122,17 @@ tlC1 = temporalLocation c1
 tlE1 = temporalLocation e1
 tlE2 = temporalLocation e2
 
--- area/window of influence as abstract quality
--- area/window of influence is a relation between an observation and a spatial/temporal region
-class ABSTRACT_QUALITY observation => INFLUENCE observation where
-	areaOfInfluence :: observation -> SpatialRegion
-	windowOfInfluence :: observation -> TemporalRegion
+-- definition of observed regions (i.e. areas and periods) for located observations
+-- observed area/period is a relation between an observation and a spatial/temporal region
+class LOCATED observation => OBSERVED_REGIONS observation where
+	observedArea :: observation -> SpatialRegion
+	observedPeriod :: observation -> TemporalRegion
 	
-instance INFLUENCE Observation where
-	-- the area of influence is defined as the spatial location of the observation
-	areaOfInfluence observation = spatialLocation observation
-	-- the window of influence is defined as the temporal location of the observation
-	windowOfInfluence observation = temporalLocation observation
-
-instance QUALITY Observation 
-instance ABSTRACT_QUALITY Observation 
+instance OBSERVED_REGIONS Observation where
+	-- the observed area is defined (in the current example) as the spatial location of the observation
+	observedArea observation = spatialLocation observation
+	-- the observed period is defined (in the current example) as the temporal location of the observation
+	observedPeriod observation = temporalLocation observation
 
 -- an observation collection has different observations as members
 -- there is no need for an id, since an observation collection is uniquely identified by its members
@@ -126,42 +145,42 @@ instance SOCIAL_OBJECTS ObservationCollection
 
 -- three examples of observation collections
 obsCollection1 = ObservationCollection {obsMembers = [b1, b2, b3, b4, b5]}
-obsCollection2 = ObservationCollection {obsMembers = [b3, c1] }
-obsCollection3 = ObservationCollection {obsMembers = [e1, e2] }
+obsCollection2 = ObservationCollection {obsMembers = [b3, c1]}
+obsCollection3 = ObservationCollection {obsMembers = [e1, e2]}
 
--- resolution as an abstract quality	
-class ABSTRACT_QUALITY observationcollection => RESOLUTION observationcollection where
-		spatialResolution :: observationcollection -> Double
-		temporalResolution :: observationcollection -> Double
+-- definition of observed study regions (i.e. areas and periods) for observation collections 
+class OBSERVED_STUDY_REGIONS observationcollection where
+		observedStudyArea :: observationcollection -> Area
+		observedStudyPeriod :: observationcollection -> Duration
+		
+instance OBSERVED_STUDY_REGIONS ObservationCollection where
+		-- the observed study area is the sum of the (distinct) observed areas of the observations
+		observedStudyArea observationcollection = srSize(observedArea(head(obsMembers observationcollection))) * (fromIntegral (length (nub (spatialregionsId observationcollection))))
+		-- the observed study period is the sum of the (distinct) observed periods of the observations
+		observedStudyPeriod observationcollection = trSize(observedPeriod(head(obsMembers observationcollection))) * (fromIntegral (length (nub (temporalregionsId observationcollection))))
 
-instance QUALITY ObservationCollection 
-instance ABSTRACT_QUALITY ObservationCollection 	
+-- the specification of the resolution of an observation collection necessitates the existence of an observed study region for the observation collection
+class OBSERVED_STUDY_REGIONS observationcollection => OBSERVATION_COLLECTION_RESOLUTIONS observationcollection where
+		spatialResolution :: observationcollection -> Area
+		temporalResolution :: observationcollection -> Duration
 
-instance RESOLUTION ObservationCollection where
-		 -- spatial resolution as the observed area of the observation collection
-         spatialResolution observationcollection = observedArea observationcollection
-		 -- temporal resolution as the observed period of the observation collection
-         temporalResolution observationcollection = observedPeriod observationcollection
-		 
--- the observed area is the sum of the (distinct) area of influences of the observations
-observedArea :: ObservationCollection -> Double
-observedArea observationcollection = srSize(areaOfInfluence(head(obsMembers observationcollection))) * (fromIntegral (length (nub (spatialregionsId observationcollection))))
+instance OBSERVATION_COLLECTION_RESOLUTIONS ObservationCollection where
+		 -- spatial resolution as the observed study area of the observation collection
+         spatialResolution observationcollection = observedStudyArea observationcollection
+		 -- temporal resolution as the observed study period of the observation collection
+         temporalResolution observationcollection = observedStudyPeriod observationcollection
 
--- the observed period is the sum of the (distinct) windows of influence of the observations
-observedPeriod :: ObservationCollection -> Double
-observedPeriod observationcollection = trSize(windowOfInfluence(head(obsMembers observationcollection))) * (fromIntegral (length (nub (temporalregionsId observationcollection))))
-
--- the function returns all the spatial regions which play the role of 'area of influence' for each of the observation in an observation collection
+-- the function returns all the spatial regions which play the role of 'observed area' for each of the observation in an observation collection
 spatialregionsId :: ObservationCollection -> [Id]
 spatialregionsId observationcollection = if null (tail (obsMembers observationcollection))
-										 then srId(areaOfInfluence(head(obsMembers observationcollection))):[]
-										 else srId(areaOfInfluence(head(obsMembers observationcollection))) : spatialregionsId ObservationCollection {obsMembers = tail (obsMembers observationcollection)}
+										 then srId(observedArea(head(obsMembers observationcollection))):[]
+										 else srId(observedArea(head(obsMembers observationcollection))) : spatialregionsId ObservationCollection {obsMembers = tail (obsMembers observationcollection)}
 										 
--- the function returns all the temporal regions which play the role of 'window of influence' for each of the observation in an observation collection							 
+-- the function returns all the temporal regions which play the role of 'observed period' for each of the observation in an observation collection							 
 temporalregionsId :: ObservationCollection -> [Id]
 temporalregionsId observationcollection = if null (tail (obsMembers observationcollection))
-										  then trId(windowOfInfluence(head(obsMembers observationcollection))):[]
-										  else trId(windowOfInfluence(head(obsMembers observationcollection))) : temporalregionsId ObservationCollection {obsMembers = tail (obsMembers observationcollection)}
+										  then trId(observedPeriod(head(obsMembers observationcollection))):[]
+										  else trId(observedPeriod(head(obsMembers observationcollection))) : temporalregionsId ObservationCollection {obsMembers = tail (obsMembers observationcollection)}
 
 -- some examples of spatial and temporal resolution for an observation collection
 sr1 = spatialResolution obsCollection1
